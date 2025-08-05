1e9b6be9174722a2c7340f655be536fdb1dbd0fb
Faical EL GOUIJ <faical.el-gouij@grenoble-inp.org>
2025-08-05 19:02:48 +0200

    i've added square and polygon classes

--- mobjects/mobjects.py
+++ mobjects/mobjects.py
@@ -158,6 +158,87 @@ class VMobject(Mobject):
     def set_stroke_color(self, color):
         self.stroke_color = color
 
+class Line(VMobject):
+    """
+    a class for dealing with lines
+    """
+
+    def __init__(self,start_pt,end_pt):
+        super().__init__()
+        self.start_pt = start_pt
+        self.end_pt = end_pt
+
+    def tangent(self):
+        #returning the slope of the line
+        dx = self.end_pt[0] - self.start_pt[0]
+        dy = self.end_pt[1] - self.start_pt[1]
+        if dx:
+            return dy/dx
+        else:
+            return np.inf
+        
+    def extend(self,x_lim_left,y_lim_down,x_lim_right,y_lim_up):
+        if self.tangent == np.inf:#vertical line
+            self.start_pt = np.array([self.start_pt[0],y_lim_down])
+            self.end_pt = np.array([self.end_pt[0],y_lim_up])
+            return
+        
+        a= self.tangent()
+        b = self.start_pt[1] - a*self.start_pt[0]
+
+        if a >0 :
+            #line equation goes y = tangent*x + (y0 - tangent*x0)
+            if a*x_lim_right + b > y_lim_up:
+                #line will pass by top
+                self.end_pt = np.array([(y_lim_up-b)/a,y_lim_up])
+
+            else:
+               self.end_pt = np.array([x_lim_right,a*x_lim_right + b]) 
+
+            if a*x_lim_left + b < y_lim_down:
+                #line will pass by top
+                self.start_pt = np.array([(y_lim_down-b)/a,y_lim_down])
+
+            else:
+               self.start_pt = np.array([x_lim_left,a*x_lim_left + b]) 
+
+        elif a <0 :
+        #line equation goes y = tangent*x + (y0 - tangent*x0)
+            if a*x_lim_right + b< y_lim_down:
+                #line will pass by top
+                self.end_pt = np.array([(y_lim_down-b)/a,y_lim_down])
+
+            else:
+                self.end_pt = np.array([x_lim_right,a*x_lim_right + b]) 
+
+            if a*x_lim_left + b > y_lim_up:
+                #line will pass by top
+                self.start_pt = np.array([(y_lim_up-b)/a,y_lim_up])
+
+            else:
+               self.start_pt = np.array([x_lim_left,a*x_lim_left + b])
+
+        else : 
+            self.start_pt = np.array([x_lim_left,b])
+            self.end_pt = np.array([x_lim_right,b])
+
+    def is_valid_line(self):
+        if self.start_pt == self.end_pt:
+            return False
+        
+        for point in self.points:
+            if not self.start_pt[0] <= point[0] <= self.end_pt[0]:
+                #if we want a segment
+                return False
+            
+            a = self.tangent()
+
+            if a != np.inf:#the other case is checked
+                dx = point[0] - self.start_pt[0]
+                dy = point[1] - self.start_pt[1]
+                if dy/dx != a:
+                    return False
+        return True
 
 class Circle(VMobject):
 
@@ -213,27 +294,39 @@ class Circle(VMobject):
 
 
 class Square(VMobject):
-    def __init__(self, side_len, **settings):
+    def __init__(self,center, side_len, **settings):
         super().__init__(**settings)
         self.close()
-        self.generate_square(side_len)
+        self.generate_square(center, side_len)
     
-    def generate_square(self, side_len):
+    def generate_square(self, center, side_len = 2.0):
         half = side_len/2
-
         # Define corners
-        p1 = np.array([-half, half])
-        p2 = np.array([-half, half])
-        p3 = np.array([-half, half])
-        p4 = np.array([-half, half])
+        corners = [
+        np.array([-half, half]) + center,
+        np.array([half, half]) + center,
+        np.array([half, -half]) + center,
+        np.array([-half, -half]) + center
+        ]
+        self.set_corners(corners)
 
-        # Create the sides
-        l1 = line(p1, p2)
-        l1 = line(p2, p3)
-        l3 = line(p3, p4)
-        l4 = line(p4, p1)
 
+class Polygon(VMobject):
+    def __init__(self, center=np.array([0,0]), radius = 2.0, n = 8.0 , **settings):
+        super().__init__(**settings)
+        self.close()
+        self.generate_polygon(center, n, radius)
         
+    def generate_polygon(self, center, n, radius):
+        corners = [np.array([0, radius])]
+        theta = 2*pi/n
+        Rotation = np.array([[np.cos(theta), -np.sin(theta)],
+                                    [np.sin(theta), np.cos(theta)],])
         
-
+        for i in range(1, n):
+            corners.append = (Rotation @ corners[i - 1].T).T
         
+        for i in range(len(corners)):
+            corners[i] += center
+            
+        self.set_corners(corners)
