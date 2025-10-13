still  need to fix scale_continuous so it could function properly in the case of two successif scaling of a GlowingDot

dot.py
@@ -5,6 +5,8 @@ from mobjects.mobjects import Circle, Square, Polygon, Dot, GlowingDot, Point, G
 from animations.animation import Transform, Move, Rotate, Scale, ColorChange, FadeIn, FadeOut, Fade
 from math import pi
 from utilities.color import *
+from utilities.rate_functions import *
+from animations.updaters import *
 
 WIDTH, HEIGHT, FPS = 4096, 2160, 60
 # Create Cairo surface and context
@@ -30,6 +32,9 @@ class dot_scene(Scene):
         group = VGroup(*mobjects) """
         """ self.add(circle) """
         print("Dot color", dot.fill_color)
+
+        dot.add_updater(scale_continuous)
+
         self.add(dot)
         self.wait(1)
         self.play(ColorChange(dot, target_fill_color= BLUE))
@@ -38,13 +43,21 @@ class dot_scene(Scene):
         self.wait(1)
         self.play(ColorChange(dot, target_fill_color= RED))
         print("Dot color", dot.fill_color)
-        self.wait(1)
+        self.wait(1) 
         
         self.play(Move(dot, np.array([3*WIDTH/4, HEIGHT/2])))
-        self.play(Move(dot, np.array([WIDTH/2, HEIGHT/2])))
+        self.play(Move(dot, np.array([WIDTH/2, HEIGHT/2]))) 
 
         self.wait(1)
-        self.play(Scale(dot, 3)) 
+        self.play(Scale(dot, 3))
+        self.play(Scale(dot, 1.5))
+
+        for i in range(100):
+            dot.run_updates(dt = 4/100, scale_speed = 2/4, iteration = i)
+            self.render_frame("output", True)
+
+        self.wait(1)
+
 
 
         self.wait(1)

groups.py
@@ -25,7 +25,7 @@ class Group_scene(Scene):
 
         Double_star.add_updater(rotate_continuous)
         Double_star.add_updater(move_continuous)
-        Double_star.add_updater(scale_continous)
+        Double_star.add_updater(scale_continuous)
         print(Double_star.updaters)
         center = Double_star.get_center()
 
@@ -38,7 +38,6 @@ class Group_scene(Scene):
         self.play(Rotate(Double_star, 2*pi, rate_time= 4))
         self.play(Scale(dot1, 1.5))
         for i in range(100):
-            dt = 4/100
             Double_star.run_updates(dt = 4/100, ang_speed = (np.pi)/2, lin_speed = 200, direction = right, scale_speed =1/100, iteration = i)
            
             self.render_frame("output", True)

updt.py
@@ -20,7 +20,7 @@ class non_slip_bearing(Scene):
         square = Square(center = np.array([WIDTH/2, HEIGHT/2]),side_len= 100.0, fill_color = BLUE, stroke_color= BLUE)
         square.add_updater(move_continuous)
         square.add_updater(rotate_continuous)
-        square.add_updater(scale_continous)
+        square.add_updater(scale_continuous)
         self.add(square)
         self.wait(1)
         for i in range(100):